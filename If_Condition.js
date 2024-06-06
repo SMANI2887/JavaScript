ccde3a539a0e69dd4a95651bda929f2e70426f6e
MANIKANDAN S <smani2887@gmail.com>
2024-06-06 16:45:13 +0530

    if conditional statement

---
+++ If_Condition.js
@@ -0,0 +1,24 @@
+// https://github.com/SMANI2887/JavaScript.git
+
+/* 1. Using if Statement
+ The if statement is used to evaluate a particular condition. If the condition holds true, the associated code block is executed.
+
+ Syntax: */
+
+if ( condition ) {
+    // If the condition is met, 
+    //code  will get executed.
+}
+
+// Example: In this example, we are using the if statement to find given number is even or odd.
+
+let num = 20;
+
+if (num % 2 === 0) {
+    console.log("Given number is even number.");
+}
+
+if (num % 2 !== 0) {
+    console.log("Given number is odd number.");
+};
+
