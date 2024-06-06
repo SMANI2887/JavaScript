f3305d4a7569be76b5314340a183065f8f7b6211
MANIKANDAN S <smani2887@gmail.com>
2024-06-06 17:24:39 +0530

    Ternary operator conditional statement

--- If_Condition.js
+++ If_Condition.js
@@ -1,49 +1,18 @@
 // https://github.com/SMANI2887/JavaScript.git
 
-/* 4. Using Switch Statement (JavaScript Switch Case)
-As the number of conditions increases, you can use multiple else-if statements in JavaScript. but when we dealing with many conditions, the switch statement may be a more preferred option.
+/* 5. Using Ternary Operator ( ?: )
+The conditional operator, also referred to as the ternary operator (?:), is a shortcut for expressing conditional statements in JavaScript.
 
-Syntax: */
+Syntax: 
 
-switch (expression) {
-    case value1:
-        statement1;
-        break;
-    case value2:
-        statement2;
-        break;
-    // . . .
-    case valueN:
-        statementN;
-        break;
-    default:
-        statementDefault;
-};
-/* Example: In this example, we find a branch name Based on the student’s marks, this switch statement assigns a specific engineering branch to the variable Branch. The output displays the student’s branch name, */
+condition ? value if true : value if false */
 
-const marks = 85;
+// Example: In this example, we use the ternary operator to check if the user’s age is 18 or older. It prints eligibility for voting based on the condition.
 
-let Branch;
+let age = 21;
 
-switch (true) {
-    case marks >= 90:
-        Branch = "Computer science engineering";
-        break;
-    case marks >= 80:
-        Branch = "Mechanical engineering";
-        break;
-    case marks >= 70:
-        Branch = "Chemical engineering";
-        break;
-    case marks >= 60:
-        Branch = "Electronics and communication";
-        break;
-    case marks >= 50:
-        Branch = "Civil engineering";
-        break;
-    default:
-        Branch = "Bio technology";
-        break;
-}
+const result =
+    (age >= 18) ? "You are eligible to vote."
+        : "You are not eligible to vote.";
 
-console.log(`Student Branch name is : ${Branch}`);
+console.log(result);
