4b4cdc84c5a7fcaca575d4b7391817062a2be41f
MANIKANDAN S <smani2887@gmail.com>
2024-06-06 17:50:17 +0530

    for-in loop - JS

--- loop_JS.js
+++ loop_JS.js
@@ -2,26 +2,18 @@
 
 // https://www.geeksforgeeks.org/control-statements-in-javascript/?ref=next_article#approach-5-using-for-loop
 
-/* 3. JavaScript do-while Loop
-The JS do-while loop is similar to the while loop with the only difference is that it checks for the condition after executing the statements, and therefore is an example of an Exit Control Loop. It executes loop content at least once event the condition is false.
+/* 4. JavaScript for-in Loop
+JS for-in loop is used to iterate over the properties of an object. The for-in loop iterates only over those keys of an object which have their enumerable property set to “true”.
 
 Syntax
-do {
-    Statements...
+for(let variable_name in object_name) {
+    // Statement
 }
-while (condition);
-
-1.The do-while loop starts with the execution of the statement(s). There is no checking of any condition for the first time.
-2.After the execution of the statements and update of the variable value, the condition is checked for a true or false value. If it is evaluated to be true, the next iteration of the loop starts.
-3.When the condition becomes false, the loop terminates which marks the end of its life cycle.
-4.It is important to note that the do-while loop will execute its statements at least once before any condition is checked and therefore is an example of the exit control loop.
-
-Example
+Example: This example shows the use of for-in loop.
 */
-let test = 1;
+let myObj = { x: 1, y: 2, z: 3 };
+for (let key in myObj) {
+    console.log(key, myObj[key]);
+}
 
-do {
-    console.log(test);
-    test++;
-} while(test <= 5)
 
