9d6369ea379fb36c72d1ca1ef33e7a7cedb20cf1
MANIKANDAN S <smani2887@gmail.com>
2024-06-14 12:33:11 +0530

    slice method

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let arr = [1,2,3,4,5];
 
-arr.forEach((a) => console.log(a))
+let arr2 = arr.slice(0,1);
+// slice(start[index], end[index])
+let arr3 = arr.slice(4,5);
+
+let arr4 = arr.slice(0,-4);
+
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
