84221d28233da1ce1d7fc7cdc33ba1e1899e6e21
MANIKANDAN S <smani2887@gmail.com>
2024-06-14 15:28:55 +0530

    add constructor

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // class is a template or prototype or buleprint
 // defining a class
 class student {
+    // constructor is a special method
+    // that gets called whenever we create an object to this class
+    constructor(){
+        console.log('constructor is called');
+    }
 
 }
 
