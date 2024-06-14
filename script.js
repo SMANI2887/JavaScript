e0136c7240f3efedd778fbb189b6317d9f6a8578
MANIKANDAN S <smani2887@gmail.com>
2024-06-14 17:30:57 +0530

    class - argument passing

--- script.js
+++ script.js
@@ -3,20 +3,19 @@
 class student {
     // constructor is a special method
     // that gets called whenever we create an object to this class
-    constructor(){
+    constructor(name, age){
         // console.log('constructor is called');
-        this.name = 'Guest';
+        this.name = name;
+        this.age = age;
     }
 
 }
 
 // create an object
-let student2 = new student();
-let student3 = new student();
+let student2 = new student('mani', 35);
+let student3 = new student('mathan', 32);
 
-// edit / update the student name
-student2.name = 'mani';
-student3.name = 'mathan';
+
 
 console.log(student2);
 console.log(student3);
