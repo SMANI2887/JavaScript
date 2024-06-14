85225df34173146bb09b2c10e4be532e7efb6c19
MANIKANDAN S <smani2887@gmail.com>
2024-06-14 11:53:42 +0530

    forEach method

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-/*
- Type of function 
-1.anonymous 
-2.IIFE
-3.arrow
- */
-// 1. normal function
+let arr = [1,2,3,4,5];
 
-function add(a,b){
-
-    console.log(a+b);
-}
-add(13,12)
-
-// another away
-
-function add2(a,b){
-    let c = a+b;
-    return c
-}
-
-console.log(add2(13,12))
-
-
-
+arr.forEach((a) => console.log(a))
