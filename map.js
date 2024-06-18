08a04de687e21e989208db34414c4928a318c8b1
MANIKANDAN S <smani2887@gmail.com>
2024-06-18 12:30:51 +0530

    map - using odd number

--- map.js
+++ map.js
@@ -1,5 +1,10 @@
 let arr = [1,2,3,4,5];
 
 arr.map( (element, index) => {
-    console.log(index, element);
+    if(element%2 == 0){
+        console.log(element);
+    }
+    // else{
+    //     console.log(element);
+    // }
 })
