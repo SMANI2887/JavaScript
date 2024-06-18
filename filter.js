f9e75935a2a6f707182b7cc6f042940e8c142598
MANIKANDAN S <smani2887@gmail.com>
2024-06-18 16:38:28 +0530

    filter method - parameter values

--- filter.js
+++ filter.js
@@ -1,6 +1,7 @@
 const number = [1,2,3,4,5];
 
-let evenno = number.filter(function(value){
+// filter -> value: Number,index: Number, Array: Number []
+let evenno = number.filter(function(value, index, array){
     return value % 2 == 0;
 });
 
